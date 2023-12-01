1b54d1de57b17bcfc64bfac47cb2eb9ee47b24a7
Petrus Kuswandi <petruskuswandi1@gmail.com>
2023-12-01 23:13:26 +0700

    update: sidebar middleware auth admin, client

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ class UserController extends Controller
 {
     public function profile()
     {
-        dd('ini halaman profile');
+        return view('profile');
     }
 }
--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -194,41 +194,62 @@
                             <!-- END Visible only in normal mode -->
                         </div>
                         <!-- END Side User -->
-                        {{ Auth::user()->role_id }}
 
                         <!-- Side Navigation -->
                         <div class="content-side content-side-full">
                             <ul class="nav-main">
-                                <li>
-                                    <a class="{{ request()->is('dashboard') ? ' active' : '' }}" href="/dashboard">
-                                        <i class="si si-cup"></i><span class="sidebar-mini-hide">Dashboard</span>
-                                    </a>
-                                </li>
-                                <li class="nav-main-heading">
-                                    <span class="sidebar-mini-visible">VR</span><span class="sidebar-mini-hidden">Various</span>
-                                </li>
-                                <li class="{{ request()->is('pages/*') ? ' open' : '' }}">
-                                    <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-bulb"></i><span class="sidebar-mini-hide">Examples</span></a>
-                                    <ul>
-                                        <li>
-                                            <a class="{{ request()->is('pages/datatables') ? ' active' : '' }}" href="/pages/datatables">DataTables</a>
-                                        </li>
-                                        <li>
-                                            <a class="{{ request()->is('pages/slick') ? ' active' : '' }}" href="/pages/slick">Slick Slider</a>
-                                        </li>
-                                        <li>
-                                            <a class="{{ request()->is('pages/blank') ? ' active' : '' }}" href="/pages/blank">Blank</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-main-heading">
-                                    <span class="sidebar-mini-visible">MR</span><span class="sidebar-mini-hidden">More</span>
-                                </li>
-                                <li>
-                                    <a href="/">
-                                        <i class="si si-globe"></i><span class="sidebar-mini-hide">Landing</span>
-                                    </a>
-                                </li>
+                                @if (Auth::user()->role_id == 1)
+                                    <li>
+                                        <a class="{{ request()->is('dashboard') ? ' active' : '' }}" href="/dashboard">
+                                            <i class="si si-cup"></i><span class="sidebar-mini-hide">Dashboard</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-main-heading">
+                                        <span class="sidebar-mini-visible">VR</span><span class="sidebar-mini-hidden">Various</span>
+                                    </li>
+                                    <li class="{{ request()->is('pages/*') ? ' open' : '' }}">
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-bulb"></i><span class="sidebar-mini-hide">Menu</span></a>
+                                        <ul>
+                                            <li>
+                                                <a class="{{ request()->is('pages/datatables') ? ' active' : '' }}" href="/pages/datatables">Books</a>
+                                            </li>
+                                            <li>
+                                                <a class="{{ request()->is('pages/slick') ? ' active' : '' }}" href="/pages/slick">Users</a>
+                                            </li>
+                                            <li>
+                                                <a class="{{ request()->is('pages/blank') ? ' active' : '' }}" href="/pages/blank">Rent Log</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-main-heading">
+                                        <span class="sidebar-mini-visible">MR</span><span class="sidebar-mini-hidden">More</span>
+                                    </li>
+                                    <li>
+                                        <a href="/">
+                                            <i class="si si-globe"></i><span class="sidebar-mini-hide">Landing</span>
+                                        </a>
+                                    </li>
+                                @else
+                                    <li class="nav-main-heading">
+                                        <span class="sidebar-mini-visible">VR</span><span class="sidebar-mini-hidden">Various</span>
+                                    </li>
+                                    <li class="{{ request()->is('pages/*') ? ' open' : '' }}">
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-bulb"></i><span class="sidebar-mini-hide">Menu</span></a>
+                                        <ul>
+                                            <li>
+                                                <a class="{{ request()->is('pages/datatables') ? ' active' : '' }}" href="/profile">Profile</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-main-heading">
+                                        <span class="sidebar-mini-visible">MR</span><span class="sidebar-mini-hidden">More</span>
+                                    </li>
+                                    <li>
+                                        <a href="/">
+                                            <i class="si si-globe"></i><span class="sidebar-mini-hide">Landing</span>
+                                        </a>
+                                    </li>
+                                @endif
                             </ul>
                         </div>
                         <!-- END Side Navigation -->
