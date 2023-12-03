bcb18e75a1e9b159ebbe0944cfd77ca1a48e6b77
“suryabk” <surya27bk@gmail.com>
2023-12-03 14:42:47 +0700

    fix: user status auto active

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('phone');
             $table->text('address');
-            $table->string('status')->default('inactive');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
