c18e5c93ec4160d8bbff055fa160e42552835f7c
suryakusuma27 <surya27bk@gmail.com>
2023-12-03 21:41:45 +0700

    feat: users page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,6 +13,7 @@ class UserController extends Controller
     }
 
     public function index(){
-        return view('layouts.user');
+        $users =  User::where('role_id',2)->get();
+        return view('layouts.user', ['users'=>$users]);
     }
 }
