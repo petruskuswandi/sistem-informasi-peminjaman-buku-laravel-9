3edc1a97b50f1e8b71bc511e65d9accc22ca2984
suryakusuma27 <surya27bk@gmail.com>
2023-12-03 15:14:19 +0700

    feat: category pages

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
-        return view("layouts.category");
+        $categories = Category::all();
+        return view("layouts.category", ["categories"=> $categories]);
     }
 }
--- resources/views/layouts/category.blade.php
+++ resources/views/layouts/category.blade.php
@@ -4,6 +4,46 @@
 @endsection
 @section('content')
     <!-- Page Content -->
-    <h1>ini halaman category</h1>
+    <div class="content">
+        <div class="block block-rounded mb-0">
+            <div class="block-content block-content-full">
+                <div class="py-3 text-center">
+                    <h1 class="h3 fw-extrabold mb-1">
+                        Category List
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="mt-3 block block-rounded">
+            <table class="table table-hover table-vcenter">
+                <thead>
+                    <tr>
+                        <th class="text-center" style="width: 100px;"></th>
+                        <th>Name</th>
+                        <th class="text-center" style="width: 200px;">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $item)
+                        <tr>
+                            <th class="text-center" scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $item->name }}</td>
+                            <td class="text-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-secondary js-bs-tooltip-enabled" data-bs-toggle="tooltip" aria-label="Edit" data-bs-original-title="Edit">
+                                        <i class="fa fa-pencil"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-sm btn-secondary js-bs-tooltip-enabled" data-bs-toggle="tooltip" aria-label="Delete" data-bs-original-title="Delete">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </div>
     <!-- END Page Content -->
 @endsection
